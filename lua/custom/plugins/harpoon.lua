55feca47033ff234c2cc6513d5a85ecafdffc9e2
Rafal-Kow <heiterkeit-ahorn.05@icloud.com>
2024-04-08 22:42:21 +0200

    feat: Added harpoon

---
+++ lua/custom/plugins/harpoon.lua
@@ -0,0 +1,37 @@
+return {
+  'ThePrimeagen/harpoon',
+  branch = 'harpoon2',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+
+  config = function()
+    local harpoon = require 'harpoon'
+    harpoon:setup {}
+
+    vim.keymap.set('n', '<leader>ha', function()
+      harpoon:list():add()
+    end, { desc = '[A]dd to quick list' })
+    vim.keymap.set('n', '<leader>hm', function()
+      harpoon.ui:toggle_quick_menu(harpoon:list())
+    end, { desc = 'show quick [M]enu' })
+
+    vim.keymap.set('n', '<A-h>', function()
+      harpoon:list():select(1)
+    end, { desc = 'go to 1 Item' })
+    vim.keymap.set('n', '<A-j>', function()
+      harpoon:list():select(2)
+    end, { desc = 'go to 2 Item' })
+    vim.keymap.set('n', '<A-k>', function()
+      harpoon:list():select(3)
+    end, { desc = 'go to 3 Item' })
+    vim.keymap.set('n', '<A-l>', function()
+      harpoon:list():select(4)
+    end, { desc = 'go to 4 Item' })
+
+    vim.keymap.set('n', '<A-p>', function()
+      harpoon:list():prev()
+    end)
+    vim.keymap.set('n', '<A-n>', function()
+      harpoon:list():next()
+    end)
+  end,
+}
